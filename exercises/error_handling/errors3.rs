288775c00aef7c1fac7b0d40bf5aea9a9fea32c0
loxp <loxppqqxol@gmail.com>
2019-04-28 16:48:56 +0800

    do errors exercises

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -10,13 +10,18 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let result = total_cost(pretend_user_input);
 
-    if cost > tokens {
-        println!("You can't afford that many!");
+    if result.is_ok() {
+        let cost = result.unwrap();
+        if cost > tokens {
+            println!("You can't afford that many!");
+        } else {
+            tokens -= cost;
+            println!("You now have {} tokens.", tokens);
+        }
     } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+        println!("{}",result.err().unwrap())
     }
 }
 
